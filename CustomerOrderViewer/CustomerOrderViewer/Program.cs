ea5fa2bd78aae04094f6d465bcdb8e5ba5b0bccc
Andrea Godoshian <94014358+godoshian@users.noreply.github.com>
2022-09-30 16:43:41 -0400

    it aint much but its honest work

---
+++ CustomerOrderViewer/CustomerOrderViewer/Program.cs
@@ -0,0 +1,31 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System.Data.SqlClient;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;Initial Catalog=CustomerOrderViewer;Integrated Security=True");
+
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach (CustomerOrderDetailModel x in customerOrderDetailModels)
+                    {
+                        Console.WriteLine(x.FirstName + " " + x.LastName + " bought " + x.Description + " for " + x.Price);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong...\n" + e.Message);
+            }
+        }
+    }
+}
---
+++ CustomerOrderViewer/CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
@@ -0,0 +1,53 @@
+﻿using CustomerOrderViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer.Repository
+{
+    internal class CustomerOrderDetailCommand
+    {
+        private string connectionString;
+
+        public CustomerOrderDetailCommand(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public IList<CustomerOrderDetailModel> GetList()
+        {
+            List<CustomerOrderDetailModel> customerOrderDetailModels = new List<CustomerOrderDetailModel>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT CustomerOrderId, CustomerId, FirstName, LastName, [Description], Price FROM CustomerOrderDetail", conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while(reader.Read())
+                            {
+                                CustomerOrderDetailModel customerOrderDetailModel = new CustomerOrderDetailModel()
+                                {
+                                    CustomerOrderId = Convert.ToInt32(reader["CustomerOrderId"]),
+                                    CustomerId = Convert.ToInt32(reader["CustomerId"]),
+                                    FirstName = reader["FirstName"].ToString(),
+                                    LastName = reader["LastName"].ToString(),
+                                    Description = reader["Description"].ToString(),
+                                    Price = Convert.ToDecimal(reader["Price"])
+                                };
+                                customerOrderDetailModels.Add(customerOrderDetailModel);
+                            }
+                        }
+                    }
+                }
+            }
+            return customerOrderDetailModels;
+        }
+    }
+}
